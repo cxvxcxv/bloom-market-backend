d9e91e6cf6e70b7d7d5e8177d5cf9f182a0f303d
cxv <ami145388@gmail.com>
2024-07-22 16:30:09 +0500

    upd: cors origin

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get<ConfigService>(ConfigService);
 
   app.getHttpAdapter().getInstance().disable('x-powered-by');
   app.use(cookieParser());
   app.enableCors({
-    origin: ['http://localhost:3000/'],
+    origin: configService.get('ORIGIN'),
     credentials: true,
     exposedHeaders: 'set-cookie',
   });
